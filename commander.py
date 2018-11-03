4d72310e2b85ef77ac6d68c7c52d7087ec5d65a8
Mayoor Rao <mayoor.rao@gmail.com>
2018-11-03 16:21:44 -0700

    socket code fix

--- commander.py
+++ commander.py
@@ -14,7 +14,8 @@ import os
 class WebSocket(SockJSConnection):
     # clients = []
     user_client = {}
-    def __init__(self):
+    def __init__(self, **kwargs):
+        super.__init__(**kwargs)
         self.sock_key = ''
     def on_open(self, info):
         # WebSocket.clients.append(self)
