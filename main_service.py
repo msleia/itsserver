263f4999cd1245f5d392ca80de7b2dd17c1caf60
Mayoor Rao <mayoor.rao@gmail.com>
2018-11-26 20:07:45 -0800

    reward and verbal clues

--- main_service.py
+++ main_service.py
@@ -5,6 +5,7 @@ from tornado.websocket import WebSocketHandler
 from sockjs.tornado import SockJSRouter, SockJSConnection
 from admin_service import SightWordHandler
 from admin_service import SentenceHandler
+from admin_service import RewardHandler
 from google_assistant_services import GAActionHandler
 import json
 
@@ -29,6 +30,7 @@ def main():
         (r'/',MainHandler),
         (r'/sightwords', SightWordHandler),
         (r'/sentences', SentenceHandler),
+        (r'/sentences', RewardHandler),
         (r'/gaservice/command', GAActionHandler)
         ], static_path=os.path.join(root,'static'), template_path=os.path.join(root,'templates')
 
